bc37725dcfc583c5aecd743bfbd0a78d1e30aec9
johnshields <johndshields14@gmail.com>
2022-05-15 18:56:01 +0100

    Update CaptainFootsteps.cs

--- celestial-beyonds_src/Assets/Main/Scripts/Captain/CaptainFootsteps.cs
+++ celestial-beyonds_src/Assets/Main/Scripts/Captain/CaptainFootsteps.cs
@@ -6,19 +6,15 @@ namespace Main.Scripts.Captain
     {
         public AudioClip[] footsteps;
         private AudioSource _audio;
-        private GameObject _player;
 
         private void Start()
         {
-            _player = GameObject.FindGameObjectWithTag("Player");
             _audio = GetComponent<AudioSource>();
         }
-        
+
         public void FootstepSounds()
         {
-            var rb = _player.GetComponent<Rigidbody>();
-            if (rb.velocity.y != 0)
-                _audio.PlayOneShot(footsteps[Random.Range(0, footsteps.Length)], 0.1f);
+            _audio.PlayOneShot(footsteps[Random.Range(0, footsteps.Length)], 0.1f);
         }
     }
 }
