afc540629bcb27c5c06d3bcdc39d77b30e85ba54
johnshields <johndshields14@gmail.com>
2022-05-15 16:26:34 +0100

    More Melee attacks and sfx for attacks and steps.

---
+++ celestial-beyonds_src/Assets/Main/Scripts/Captain/CaptainAnimAndSound.cs
@@ -0,0 +1,113 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/*
+ * PlayerAnimAndSound
+ * Script that controls the Player's animations & sounds.
+ */
+namespace Main.Scripts.Captain
+{
+    public class CaptainAnimAndSound : MonoBehaviour
+    {
+        public float maxSpeed = 5f;
+        private Animator _animator;
+        private Rigidbody _rb;
+        private InputProfiler _controls;
+        private GameObject _player, _footsteps;
+        private int _profile, _jump, _melee0ne, _meleeTwo, _meleeThree, _meleeFour, _meleeFive;
+        private AudioSource _audio;
+        public AudioClip[] meleeSFX;
+        public float delayAttack = 1f;
+        private bool _actionDone;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody>();
+            _controls = new InputProfiler();
+        }
+
+        private void Start()
+        {
+            _player = GameObject.FindGameObjectWithTag("Player");
+            _footsteps = GameObject.FindGameObjectWithTag("Footsteps");
+            _animator = GetComponent<Animator>();
+            _audio = GetComponent<AudioSource>();
+            _profile = Animator.StringToHash("Profile");
+            _jump = Animator.StringToHash("JumpActive");
+            _melee0ne = Animator.StringToHash("Melee_1");
+            _meleeTwo = Animator.StringToHash("Melee_2");
+            _meleeThree = Animator.StringToHash("Melee_3");
+            _meleeFour = Animator.StringToHash("Melee_4");
+            _meleeFive = Animator.StringToHash("Melee_5");
+        }
+
+        private void Update()
+        {
+            _animator.SetFloat(_profile, _rb.velocity.magnitude / maxSpeed);
+        }
+
+        private void OnEnable()
+        {
+            _controls.Profiler.Jump.started += Jump;
+            _controls.Profiler.Attack.started += MeleeAttack;
+            _controls.Profiler.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _controls.Profiler.Jump.started -= Jump;
+            _controls.Profiler.Attack.started -= MeleeAttack;
+            _controls.Profiler.Disable();
+        }
+
+        private void Jump(InputAction.CallbackContext obj)
+        {
+            if (CaptainProfiler.grounded)
+                _animator.SetTrigger(_jump);
+        }
+
+        private void MeleeAttack(InputAction.CallbackContext obj)
+        {
+            var attackBool = Random.Range(0, 5);
+            if (!_actionDone)
+            {
+                switch (attackBool)
+                {
+                    case 0:
+                        _animator.SetTrigger(_melee0ne);
+                        break;
+                    case 1:
+                        _animator.SetTrigger(_meleeTwo);
+                        break;
+                    case 2:
+                        _animator.SetTrigger(_meleeThree);
+                        break;
+                    case 3:
+                        _animator.SetTrigger(_meleeFour);
+                        break;
+                    case 4:
+                        _animator.SetTrigger(_meleeFive);
+                        break;
+                }
+
+                _actionDone = true;
+                Invoke(nameof(ResetAttack), delayAttack);
+            }
+        }
+
+        private void ResetAttack()
+        {
+            _actionDone = false;
+        }
+
+        private void Footsteps()
+        {
+            _footsteps.GetComponent<CaptainFootsteps>().FootstepSounds();
+        }
+
+        private void Melee()
+        {
+            _audio.PlayOneShot(meleeSFX[Random.Range(0, meleeSFX.Length)], 0.1f);
+        }
+    }
+}
---
+++ celestial-beyonds_src/Assets/Main/Scripts/Captain/CaptainFootsteps.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace Main.Scripts.Captain
+{
+    public class CaptainFootsteps : MonoBehaviour
+    {
+        public AudioClip[] footsteps;
+        private AudioSource _audio;
+        
+        private void Start()
+        {
+            _audio = GetComponent<AudioSource>();
+        }
+        
+        public void FootstepSounds()
+        {
+            _audio.PlayOneShot(footsteps[Random.Range(0, footsteps.Length)], 0.1f);
+        }
+    }
+}
