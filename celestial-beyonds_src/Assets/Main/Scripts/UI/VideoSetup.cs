1cc68ea9e34978911fdb206b3f5b566c18be3ec8
John Shields <G00348436@gmit.ie>
2022-06-17 15:38:48 +0100

    Video for WebGL.

---
+++ celestial-beyonds_src/Assets/Main/Scripts/UI/VideoSetup.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoSetup : MonoBehaviour
+{
+    public GameObject videoPlayer;
+    public string videoURL;
+
+    private void Awake()
+    {
+        videoPlayer.GetComponent<VideoPlayer>().url =
+            System.IO.Path.Combine(Application.streamingAssetsPath, videoURL);
+    }
+}
