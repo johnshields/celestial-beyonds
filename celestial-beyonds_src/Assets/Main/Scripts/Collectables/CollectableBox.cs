ffc1d9c968d49d2fba91c585eb58dd5c07af2b5e
John Shields <johndshields14@gmail.com>
2022-07-09 11:41:51 +0100

    TRAPPIST-1 landscape done.

--- celestial-beyonds_src/Assets/Main/Scripts/Collectables/CollectableBox.cs
+++ celestial-beyonds_src/Assets/Main/Scripts/Collectables/CollectableBox.cs
@@ -1,22 +1,30 @@
 using System;
+using Main.Scripts.Captain;
 using UnityEngine;
 
 public class CollectableBox : MonoBehaviour
 {
     public AudioClip soundFX;
+    private GameObject _player;
+
+
+    private void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Scraper"))
+        if (other.CompareTag("Scraper") && _player.GetComponent<CaptainAnimAndSound>().meleeActive)
         {
             AudioSource.PlayClipAtPoint(soundFX, transform.position, 0.25f);
-            Destroy(gameObject);   
+            Destroy(gameObject);
         }
     }
 
     public void IfCannon()
     {
         AudioSource.PlayClipAtPoint(soundFX, transform.position, 0.5f);
-        Destroy(gameObject);   
+        Destroy(gameObject);
     }
 }
