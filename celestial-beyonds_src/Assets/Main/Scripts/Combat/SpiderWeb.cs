510b8cefc187dc2b5d6fd41dd787e354707a7d78
John Shields <johndshields14@gmail.com>
2022-07-17 16:47:40 +0100

    Spiderwebs updated for Player's blood Particles.,

--- celestial-beyonds_src/Assets/Main/Scripts/Combat/SpiderWeb.cs
+++ celestial-beyonds_src/Assets/Main/Scripts/Combat/SpiderWeb.cs
@@ -1,14 +1,22 @@
+using System;
 using UnityEngine;
 
 public class SpiderWeb : MonoBehaviour
 {
     private bool _webShot;
+    private ParticleSystem _playerBloodParticles;
+
+    private void Start()
+    {
+        _playerBloodParticles = GameObject.FindGameObjectWithTag("PlayerBlood").GetComponent<ParticleSystem>();
+    }
 
     private void OnParticleCollision(GameObject other)
     {
         if (!_webShot)
         {
             GameObject.FindGameObjectWithTag("Player").GetComponent<CaptainHealth>().PlayerTakeDamage(10);
+            _playerBloodParticles.Play();
             Invoke(nameof(ResetWeb), 3);
         }
     }
