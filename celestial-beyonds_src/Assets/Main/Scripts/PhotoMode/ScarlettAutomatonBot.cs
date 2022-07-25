ed317476ffac365b868dfae72e0d2c40b19dbcbd
John Shields <johndshields14@gmail.com>
2022-07-25 03:31:54 +0100

    Added PhotoGallery to TRAAPIST-1.

--- celestial-beyonds_src/Assets/Main/Scripts/PhotoMode/ScarlettAutomatonBot.cs
+++ celestial-beyonds_src/Assets/Main/Scripts/PhotoMode/ScarlettAutomatonBot.cs
@@ -1,19 +1,22 @@
 using System.IO;
 using System.Collections;
-using System.Text;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 // ref - https://github.com/Nrjwolf/UnityTelegramExample
 public class ScarlettAutomatonBot : MonoBehaviour
 {
+    public Text status;
+    private bool _done;
+    private long _code;
     public string chatID = "635500116"; // ID (you can know your id via @userinfobot)
     public string token = "5499658523:AAFlhX5FwSuBG7nYPFUelUNvUA03Yw6kX_4"; // bot token (@BotFather)
     private string _url => $"https://api.telegram.org/bot{token}/";
-
-
+    
     public void SendPhotoToGram(string path, string photoID)
     {
+        _done = false;
         GetMe();
         var bytes = File.ReadAllBytes(path + photoID);
         SendMessage(GetUserInfo() + "\n\nHi there! Check out the new Photo!" + "\nPhoto ID: \n" + photoID);
@@ -28,8 +31,8 @@ public class ScarlettAutomatonBot : MonoBehaviour
     private void GetMe()
     {
         var form = new WWWForm();
-        var www = UnityWebRequest.Post(_url + "getMe", form);
-        StartCoroutine(SendRequest(www));
+        var webRequest = UnityWebRequest.Post(_url + "getMe", form);
+        StartCoroutine(PostRequest(webRequest));
     }
 
     private void SendPhoto(byte[] bytes, string filename)
@@ -37,8 +40,8 @@ public class ScarlettAutomatonBot : MonoBehaviour
         var form = new WWWForm();
         form.AddField("chat_id", chatID);
         form.AddBinaryData("photo", bytes, filename, "filename");
-        var www = UnityWebRequest.Post(_url + "sendPhoto?", form);
-        StartCoroutine(SendRequest(www));
+        var webRequest = UnityWebRequest.Post(_url + "sendPhoto?", form);
+        StartCoroutine(PostRequest(webRequest));
     }
 
     private new void SendMessage(string text)
@@ -46,17 +49,27 @@ public class ScarlettAutomatonBot : MonoBehaviour
         var form = new WWWForm();
         form.AddField("chat_id", chatID);
         form.AddField("text", text);
-        var www = UnityWebRequest.Post(_url + "sendMessage?", form);
-        StartCoroutine(SendRequest(www));
+        var webRequest = UnityWebRequest.Post(_url + "sendMessage?", form);
+        StartCoroutine(PostRequest(webRequest));
     }
 
-    private IEnumerator SendRequest(UnityWebRequest webRequest)
+    private IEnumerator PostRequest(UnityWebRequest webRequest)
     {
         yield return webRequest.SendWebRequest();
         if (webRequest.result == UnityWebRequest.Result.ConnectionError ||
             webRequest.result == UnityWebRequest.Result.ProtocolError)
-            print(webRequest.error);
+            print("Connection to ScarlettAutomatonBot - Error: " + webRequest.error);
         else
+        {
             print("Success!\n" + webRequest.downloadHandler.text);
+            _code = webRequest.responseCode;
+        }
+        _done = true;
+    }
+
+    private void OnGUI()
+    {
+        if (_done)
+            status.text = "Success: " + _done + " -> " + _code;
     }
 }
