c372207c9d03a2e256cdb4b88a6cb1521bf387aa
John Shields <johndshields14@gmail.com>
2022-07-30 01:04:34 +0100

    Kepler-186f Design: Rain and stone henge.

--- celestial-beyonds_src/Assets/Main/Scripts/PhotoMode/ScarlettPhotographerBot.cs
+++ celestial-beyonds_src/Assets/Main/Scripts/PhotoMode/ScarlettPhotographerBot.cs
@@ -11,7 +11,7 @@ public class ScarlettPhotographerBot : MonoBehaviour
     private bool _success, _done;
     private long _code;
     private string chatID = "635500116";
-    private string token = "";
+    private string token = "54-yFxJahnblTr7Dg8c";
     private string _url => $"https://api.telegram.org/bot{token}/";
     
     public void SendPhotoToGram(string path, string photoID)
