5df692eac2cf43fc0fe6b27fcca031105c11affd
John Shields <johndshields14@gmail.com>
2022-08-05 00:35:39 +0100

    Credits Scene set up.

--- celestial-beyonds_src/Assets/Main/Scripts/Collectables/ObjectRotator.cs
+++ celestial-beyonds_src/Assets/Main/Scripts/Collectables/ObjectRotator.cs
@@ -2,12 +2,13 @@ using UnityEngine;
 
 /*
  * ObjectRotator
- * Script to rotate objects (keys + scrolls).
+ * Script to rotate objects.
 */
 public class ObjectRotator : MonoBehaviour
 {
+    public float speed = 1;
     private void Update()
     {
-        transform.Rotate(new Vector3(0f, 1f, 0f));
+        transform.Rotate(new Vector3(0f, speed, 0f));
     }
 }
